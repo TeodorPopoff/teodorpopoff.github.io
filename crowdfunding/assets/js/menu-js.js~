3450ddaf5fd81c745800cc4817a60c657e452b4d
Teodor Popov <teodor.popoff@gmail.com>
2016-12-01 18:26:50 +0200

    Previews of Crowdfunding and The Flying Penguin

---
+++ crowdfunding/assets/js/menu-js.js~
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+	$(".sub-menu").hide();
+
+	$("#dropdown-about").click(function(){
+		$(".sub-menu").show(400);
+		event.stopPropagation();
+	});
+	$('html').click(function(){
+		$('.sub-menu').hide(400);
+	});
+});
---
+++ crowdfunding/assets/js/touchScroll.js
@@ -0,0 +1,16 @@
+function touchScroll(id){
+    if(isTouchDevice()){ //if touch events exist...
+        var el=document.getElementById(id);
+        var scrollStartPos=0;
+
+        document.getElementById(id).addEventListener("touchstart", function(event) {
+            scrollStartPos=this.scrollTop+event.touches[0].pageY;
+            event.preventDefault();
+        },false);
+
+        document.getElementById(id).addEventListener("touchmove", function(event) {
+            this.scrollTop=scrollStartPos-event.touches[0].pageY;
+            event.preventDefault();
+        },false);
+    }
+}
